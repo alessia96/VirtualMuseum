7c16c78b41a87e200bc0195edea0afa1476ec20e
Alessia <alessia.nigretti@gmail.com>
2019-05-02 18:52:23 +0100

    Updated php

---
+++ 3dapp/assignment/application/model/model.php
@@ -0,0 +1,16 @@
+<?php
+
+class Model {
+
+    public function model3D_info()
+    {
+        return array(
+            'model_1' => 'Coke Can 3D Image 1',
+            'image3D_1' => 'gun',
+
+            'model_2' => 'Coke Can 3D Image 2',
+            'image3D_2' => 'svd',
+		);
+	}
+}
+?>
